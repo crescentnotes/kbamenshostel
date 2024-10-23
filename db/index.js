180e4c8e7c66acc0ee3590425da43c5be068c92f
irfan mohamed <crescentnotes53@gmail.com>
2024-10-23 18:07:53 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -4,11 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();  // Import the default export
 const { Pool } = pkg; // Extract Pool from the imported package
 
- const pool = new Pool({
-  user: 'postgres.rqibetocqeuusrgsjrhi',  // Your database username
-  host: 'aws-0-ap-southeast-1.pooler.supabase.com',  // Host UR;
+const pool = new pool({
+     user: 'postgres.rqibetocqeuusrgsjrhi',  // Your database username
+  host: 'aws-0-ap-southeast-1.pooler.supabase.com',  // Host URL
   database: 'postgres',  // Database name
   password: 'Irfan@0132Irfan',  // Replace with your actual password
-  port: 6543,  // Port number
+  port: 6543,                 // default port for PostgreSQL
 });
+
 export default pool;
